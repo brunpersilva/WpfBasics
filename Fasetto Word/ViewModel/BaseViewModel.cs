8194cb67133f0e1957c4ded1831388c751ddbdb2
Bruno Silva <brunpersilva@gmail.com>
2021-03-10 19:41:41 -0300

    Added a new Wpf Project

---
+++ Fasetto Word/ViewModel/BaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using PropertyChanged;
+
+namespace Fasetto_Word
+{
+    /// <summary>
+    /// Base Viewmodel that fires property changed as needed
+    /// </summary>
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The event that is fired when any child property changes its value
+        /// </summary>
+
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        /// <summary>
+        /// Call this to fire a event
+        /// </summary>
+        /// <param name="name"></param>
+        public void OnpropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
